b6aaa29cfacb851b5bf979a315579f97a3473548
Phil Nash <philnash@gmail.com>
2019-10-08 14:15:23 +1100

    Renames Login to Lobby (as it should have been)

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Login = ({
+const Lobby = ({
   username,
   handleUsernameChange,
   roomName,
@@ -37,4 +37,4 @@ const Login = ({
   );
 };
 
-export default Login;
+export default Lobby;

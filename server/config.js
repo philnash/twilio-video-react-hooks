d288ff0b65a19cfa5945faa50faccf01e2957e48
Phil Nash <philnash@gmail.com>
2020-03-28 13:21:12 +1100

    Things for deploying to Heroku:
    Added npm script for running express server in prod.
    Added Procfile to run server on Heroku.
    Pinned Node to version 12.
    Serve build directory from Express.

--- server/config.js
+++ server/config.js
@@ -3,5 +3,6 @@ module.exports = {
     accountSid: process.env.TWILIO_ACCOUNT_SID,
     apiKey: process.env.TWILIO_API_KEY,
     apiSecret: process.env.TWILIO_API_SECRET
-  }
+  },
+  port: process.env.PORT || 3001
 };
--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const config = require('./config');
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
-const { videoToken } = require('./tokens');
+const path = require("path");
+const config = require("./config");
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+const { videoToken } = require("./tokens");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 const sendTokenResponse = (token, res) => {
-  res.set('Content-Type', 'application/json');
+  res.set("Content-Type", "application/json");
   res.send(
     JSON.stringify({
       token: token.toJwt()
@@ -18,26 +20,25 @@ const sendTokenResponse = (token, res) => {
   );
 };
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
+app.get("/api/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.get('/video/token', (req, res) => {
+app.get("/video/token", (req, res) => {
   const identity = req.query.identity;
   const room = req.query.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
-
 });
-app.post('/video/token', (req, res) => {
+app.post("/video/token", (req, res) => {
   const identity = req.body.identity;
   const room = req.body.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+app.listen(config.port, () =>
+  console.log(`Express server is running on localhost:${config.port}`)
 );

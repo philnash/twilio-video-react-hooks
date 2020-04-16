917b232264a7d457f969f43ea7c8a0857db04219
Phil Nash <philnash@gmail.com>
2020-04-16 20:46:10 +1000

    Removes comment about data tracks

--- src/Participant.js
+++ src/Participant.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 const Participant = ({ participant }) => {
   const [videoTracks, setVideoTracks] = useState([]);
@@ -7,33 +7,33 @@ const Participant = ({ participant }) => {
   const videoRef = useRef();
   const audioRef = useRef();
 
-  const trackpubsToTracks = trackMap => Array.from(trackMap.values())
-    .map(publication => publication.track)
-    .filter(track => track !== null);
+  const trackpubsToTracks = (trackMap) =>
+    Array.from(trackMap.values())
+      .map((publication) => publication.track)
+      .filter((track) => track !== null);
 
   useEffect(() => {
     setVideoTracks(trackpubsToTracks(participant.videoTracks));
     setAudioTracks(trackpubsToTracks(participant.audioTracks));
 
-    const trackSubscribed = track => {
-      // track.kind === 'data' for DataTracks
-      if (track.kind === 'video') {
-        setVideoTracks(videoTracks => [...videoTracks, track]);
-      } else if (track.kind === 'audio') {
-        setAudioTracks(audioTracks => [...audioTracks, track]);
+    const trackSubscribed = (track) => {
+      if (track.kind === "video") {
+        setVideoTracks((videoTracks) => [...videoTracks, track]);
+      } else if (track.kind === "audio") {
+        setAudioTracks((audioTracks) => [...audioTracks, track]);
       }
     };
 
-    const trackUnsubscribed = track => {
-      if (track.kind === 'video') {
-        setVideoTracks(videoTracks => videoTracks.filter(v => v !== track));
-      } else if (track.kind === 'audio'){
-        setAudioTracks(audioTracks => audioTracks.filter(a => a !== track));
+    const trackUnsubscribed = (track) => {
+      if (track.kind === "video") {
+        setVideoTracks((videoTracks) => videoTracks.filter((v) => v !== track));
+      } else if (track.kind === "audio") {
+        setAudioTracks((audioTracks) => audioTracks.filter((a) => a !== track));
       }
     };
 
-    participant.on('trackSubscribed', trackSubscribed);
-    participant.on('trackUnsubscribed', trackUnsubscribed);
+    participant.on("trackSubscribed", trackSubscribed);
+    participant.on("trackUnsubscribed", trackUnsubscribed);
 
     return () => {
       setVideoTracks([]);
